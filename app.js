06824c0be179ce9de3267ca8d34745f329f56eb6
irina.taran17@gmail.com <irina.taran17@gmail.com>
2019-01-06 00:31:53 +0200

    Added PUT:id method and made some refactoring

--- app.js
+++ app.js
@@ -21,23 +21,44 @@ app.get('/fortunes/:id', (req, res) => {
     res.json(fortunes.find(f => f.id == req.params.id));
 });
 
+const writeFortunes = (json) => {
+    fs.writeFile('./data/fortunes.json', JSON.stringify(json), err => console.log(err));
+};
+
 app.post('/fortunes', (req, res) => {
     const {message, lucky_number, spirit_animal} = req.body;
 
     const fortune_ids = fortunes.map(f => f.id);
 
-    const fortune = {
+    const new_fortunes = fortunes.concat({
         id: (fortune_ids.length > 0 ? Math.max(...fortune_ids) : 0) + 1,
         message,
         lucky_number,
         spirit_animal
-    };
+    });
 
-    const new_fortunes = fortunes.concat(fortune);
-
-    fs.writeFile('./data/fortunes.json', JSON.stringify(new_fortunes), err => console.log(err));
+    writeFortunes(new_fortunes);
 
     res.json(new_fortunes);
+});
+
+app.put('/fortunes/:id', (req, res) => {
+    const {id} = req.params;
+    // const {message, lucky_number, spirit_animal} = req.body;
+
+    const old_fortune = fortunes.find(f => f.id == id);
+
+    // if (message) old_fortune.message = message;
+    // if (lucky_number) old_fortune.lucky_number = lucky_number;
+    // if (spirit_animal) old_fortune.spirit_animal = spirit_animal;
+
+    ['message', 'lucky_number', 'spirit_animal'].forEach(key => {
+        if (req.body[key]) old_fortune[key] = req.body[key];
+    });
+
+    writeFortunes(fortunes);
+
+    res.json(fortunes);
 });
 
 module.exports = app;
